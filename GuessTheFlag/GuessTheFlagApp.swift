dc56c06cf05292e67ceccc230084ef14f06670c3
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-03-06 14:03:46 +0100

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Hossein Sharifi on 06/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
